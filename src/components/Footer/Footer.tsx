f27beb08a8a0420bc2cbe8e0c629820bbc7c0fc8
babafakruddin-babanbai <babanbaibabafakruddin@gmail.com>
2023-01-11 23:10:53 +0530

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterContainer, FooterText } from "./FooterStyle";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <FooterText>
+        This is acoincap.io clone
+      </FooterText>
+    </FooterContainer>
+  );
+};
+
+export { Footer };
